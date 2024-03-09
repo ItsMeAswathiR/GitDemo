be0640c04d3a79e2fa7e9dad69ef48d1100e42c6
Aswathi <aswathiepic7@gmail.com>
2024-03-09 17:26:17 +0530

    dropdown push 2tym

--- src/test/java/seleniumpractice/Updated_dropdown.java
+++ src/test/java/seleniumpractice/Updated_dropdown.java
@@ -12,14 +12,18 @@ public class Updated_dropdown {
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		//to open dropdown and click plus button
 		driver.findElement(By.id("divpaxinfo")).click();
-		//Thread.sleep(2000);
-		//driver.findElement(By.id("hrefIncAdt")).click();
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		Thread.sleep(2000);
+		
 		int i=1;
 		while(i<5)
 		{
 			driver.findElement(By.id("hrefIncAdt")).click();
 			i++;
 		}
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		
 	}
 
 }
